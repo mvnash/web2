03a5a6101f97c830a3f18701e5c9649048df8057
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2022-10-03 09:02:21 +0200

    myMovies : module 2 : dynamic homepage

--- module2/2.1/src/index.js
+++ module2/2.1/src/index.js
@@ -1,2 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+import wallpaper from './img/mov_wallpaper.jpg';
+import prisonBreakImage from './img/pb.jpg';
+
+renderImage(wallpaper);
+renderImage(prisonBreakImage);
+
+const h1 = document.createElement('h1');
+h1.innerText = 'MyMovies';
+document.querySelector('header').appendChild(h1);
+
+function renderImage(image) {
+  const img = document.createElement('img');
+  img.src = image;
+  document.querySelector('main').appendChild(img);
+}
